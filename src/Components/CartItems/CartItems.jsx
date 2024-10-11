9f6a2c58409f2d7ea43b66a2e0ad225be7b4ef8a
Mohammed Absar <absar6152@gmail.com>
2024-10-11 20:55:04 +0530

    2nd commit front-end

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,9 +4,9 @@ import { ShopContext } from '../Context/ShopContext';
 import remove_icon from'../Assets/cart_cross_icon.png';
 
 const CartItems = () => {
-    const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+    const {getTotalCartAmount,all_product, cartItems, removeFromCart } = useContext(ShopContext);
     return (
-        <div className='cartitems'>
+        <div className='cartitems '>
             <div className="cartitems-format-main">
                 <p>Products</p>
                 <p>Title</p>
@@ -20,13 +20,13 @@ const CartItems = () => {
                 if (cartItems[e.id] > 0) {
                     return (
                         <div key={e.id}>
-                            <div className="cartitems-format">
+                            <div className="cartitems-format cartitems-format-main">
                                 <img src={e.image} alt="" className='carticon-product-icon' />
                                 <p>{e.name}</p>
                                 <p>${e.new_price}</p>
                                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
                                 <p>${(e.new_price * cartItems[e.id])}</p>
-                                <img src={remove_icon} onClick={() => removeFromCart(e.id)} alt="Remove item" />
+                                <img src={remove_icon} className='cartitems-remove-icon' onClick={() => removeFromCart(e.id)} alt="Remove item" />
                             </div>
                             <hr />
                         </div>
@@ -35,6 +35,36 @@ const CartItems = () => {
                     return null;
                 }
             })}
+            <div className="cartitems-down">
+                <div className="cartitem-total">
+                    <h1>Cart Total</h1>
+                    <div>
+                        <div className="cartitems-total-item">
+                            <p>Subtotal</p>
+                            <p>${getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <p>Shipping fee</p>
+                            <p>Free</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <h3>Total</h3>
+                            <h3>{getTotalCartAmount()}</h3>
+                        </div>
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+                <div className="cartitems-promocode">
+                    <p>if you have promocode, enter it here</p>
+                    <div className="promocode-box">
+                        <input type="text" placeholder="Enter your promo code" />
+                        <button>APPLY</button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Navbar.css';
-
 import logo from '../Assets/e-logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 import { Link } from 'react-router-dom';
+import { ShopContext } from '../Context/ShopContext';
 
 const Navbar = () => {
 
     const [menu,setMenu] = useState("home");
+    const {getTotalCartItems} = useContext(ShopContext);
 
 
 
@@ -27,7 +28,7 @@ const Navbar = () => {
                 <Link to='/login'><button>Login</button></Link>
                 <Link to='/cart'><img src={cart_icon} alt="cart icon" /></Link>
 
-                <div className="nav-cart-count">0</div>
+                <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
             
         </div>
